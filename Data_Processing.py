66650c1e6d2bc4a3a88953412ea38011dc13ba76
CuratorJin <804242129@qq.com>
2023-04-30 09:42:57 +0800

    Mutation Optimize

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,7 +1,10 @@
 import os
 import numpy as np
+import time
 data_dir='Samples'
+mapdata_dir='mapData'
 def delete(data_dir):
+    """ delte files while are too small """
     seed_names = os.listdir(data_dir)
     avg_size=0
     rate=0.1
@@ -10,24 +13,40 @@ def delete(data_dir):
         avg_size+=tmp_size
     avg_size/=len(seed_names)
     del_num=0
-    for name in seed_names:#删除过小的文件
+    for name in seed_names:
         tmp_size=os.path.getsize(data_dir+'/'+name)
         if tmp_size<avg_size*rate:
             os.remove(data_dir+'/'+name)
             del_num+=1
     print('finish:delete %d files'%(del_num))
 
-def vectorize(fname):
-    """ 读取文件并向量化 """
+def vectorize_from_file(fname):
+    """ get the file and vectorize """
     v=list(open(fname,'rb').read())
     return np.array(v)
 
+
+def padding(f,size):
+    for _ in range(size-len(f)):
+        f.append(0)
+    return f
+
+def get_maxsize(dirname):
+    maxsize=0
+    seed_names = os.listdir(dirname)
+    for name in seed_names:
+        tmp_size=os.path.getsize(dirname+'/'+name)
+        if tmp_size>maxsize:
+            maxsize=tmp_size
+    return maxsize
+
 def mutate_pos(v1,v2):
     """
-    用于获取变异位置：v1为原种子，v2为变异后文件，输入均需要向量化
-    返回0-1构成的向量，1代表此位置发生变异
-    要求v1和v2长度相等
-    例如:v1=[1 2 3]  v2=[1 2 4]，返回 [0 0 1]
+    get the mutate position, len(v1) should be equal to len(v2)
+    :param v1: vector1
+    :param v2: vector2
+    :return z 0-1 vector
+    expample: v1=[1 2 3]  v2=[1 2 4]  z= [0 0 1]
     """
     if len(v1)!=len(v2):
         print("length of v1 should be equal to v2")
@@ -47,21 +66,68 @@ def get_cov(dirname):
             y[i]=1
     return y
 
+
 def get_x(dirname):
     x=[]
+    if os.path.exists(dirname)==False:
+        print('File not exists!')
+        exit(-1)
     seed_names=os.listdir(dirname)
-    orig_file=open(dirname+'/'+seed_names[0],'rb').read()
-    for i in range(1,len(dirname)):
-        cur_file=open(dirname+'/'+seed_names[i],'rb').read()
-        x.append(mutate_pos(orig_file,cur_file))
-    return x
+    max_size=get_maxsize(dirname)
+    orig_file=list(open(dirname+'/'+seed_names[0],'rb').read())
+    orig_file=np.array(padding(orig_file,max_size))
+    for i in range(len(seed_names)):
+        cur_file=list(open(dirname+'/'+seed_names[i],'rb').read())
+        padding(cur_file,max_size)
+        x.append(mutate_pos(orig_file,np.array(cur_file)))
+    return x,max_size
 
+def get_Bitmap_data(dirname):
+    bitmap_data=[]
+    seed_names=os.listdir(dirname)
+    orig_bitmap=open(dirname+'/'+seed_names[0],'rb').read()
+    for i in range(50):
+        cur_bitmap=open(dirname+'/'+seed_names[i],'rb').read()
+        cur=0
+        for i in range(0x10000):
+            for j in range(8):
+                if (cur_bitmap[i]>>j)&1 == 1 and (orig_bitmap[i]>>j)&1 == 0:
+                    cur+=1
+        bitmap_data.append(cur)
+    return bitmap_data
+
+def get_Bitmap_data_fast(dirname):
+    bitmap_data=[]
+    seed_names=os.listdir(dirname)
+    orig_bitmap=open(dirname+'/'+seed_names[0],'rb').read()
+    for i in range(len(seed_names)):
+        cur_bitmap=open(dirname+'/'+seed_names[i],'rb').read()
+        cur=0
+        for j in range(0x10000):
+            tmp = (cur_bitmap[j]^orig_bitmap[j])&cur_bitmap[j]
+            cur += bin(tmp).count('1')
+        bitmap_data.append(cur)
+    return bitmap_data
+""" 
+a<b:
+(a^b)&a
+"""
 def test():
-    v1=vectorize('small_exec.elf')
-    v2=vectorize('small_exec2.elf')
-    print(v1,v2)
-    print(mutate_pos(v1,v2))
-    print(get_cov('Samples'))
+    # v1=vectorize_from_file('small_exec.elf')
+    # v2=vectorize_from_file('small_exec2.elf')
+    # print(v1,v2)
+    # print(mutate_pos(v1,v2))
+    # print(get_cov('Samples'))
+    # print(get_x('Samples'))
+    time1=time.time()
+    bit_map_geq=get_Bitmap_data_fast(mapdata_dir)
+    time2=time.time()
+    print(time2-time1)
+    open('bit_map_geq','w').write(str(bit_map_geq))
+    # print(get_Bitmap_data(mapdata_dir))
+    # time3=time.time()
+    # print(time3-time2)
     pass
 
-print(get_x('Samples'))
+if __name__=='__main__':
+    test()
--- Mutation_Optimize_LSTM.py
+++ Mutation_Optimize_LSTM.py
@@ -4,38 +4,64 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import random_split
+import torch.utils.data as data
 import Data_Processing
-device=device = "cuda:0" if torch. cuda.is_available() else "cpu"
-data_dir=""
+from torchvision import transforms
+
+import matplotlib.pyplot as plt
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+data_dir="Samples"
+bitmap_file='bit_map_geq'
 
 max_filesize=0
 seed_names = os.listdir(data_dir)
 for name in seed_names:
     tmp_size=os.path.getsize(data_dir+'/'+name)
-    if tmp_size>maxsize:
-        maxsize=tmp_size
+    if tmp_size>max_filesize:
+        max_filesize=tmp_size
 
 # Define function hyperparameters
-epochs=100
+epochs=8000
 batch_size=32
-lr=0.0005
+lr=0.001
+input_dim=max_filesize
+n_layer=3
+hidden_dim=550
+n_class=1
 
-class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
-        super(LSTM, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device=x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device=x.device)
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
+class LSTMnet(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_layer, n_class):
+        super(LSTMnet, self).__init__()
+        self.n_layer = n_layer
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(in_dim, hidden_dim, n_layer, batch_first=True)
+        self.linear = nn.Linear(hidden_dim, n_class)
+ 
+    def forward(self, x):                  
+        out, _ = self.lstm(x)              
+        out = out[:, -1, :]                
+                                           
+        out = self.linear(out)             
         return out
+ 
+## Data loading...
 
+class MyDataset(data.Dataset):
+    def __init__(self, samples, bitmaps, transform=None):
+        self.data=torch.tensor(samples,dtype=torch.float).reshape(len(samples),1,-1)
+        self.labels=torch.tensor(bitmaps,dtype=torch.float).reshape(len(bitmaps),1,-1)
+        labels_mean=torch.mean(self.labels)
+        labels_std=torch.std(self.labels)
+        self.labels=(self.labels-labels_mean)/labels_std
+    
+    def __getitem__(self, index):
+        return self.data[index],self.labels[index]
+    
+    def __len__(self):
+        return len(self.data)
     
 def get_dataloader(batch_size, data, targets):
     """
@@ -45,7 +71,7 @@ def get_dataloader(batch_size, data, targets):
     :return: DataLoader with batched data
     """
 
-    seed_data = TensorDataset(data,targets)
+    seed_data = MyDataset(data,targets)
     
     data_loader = torch.utils.data.DataLoader(seed_data,
                                           batch_size,
@@ -53,22 +79,68 @@ def get_dataloader(batch_size, data, targets):
     return data_loader
 
 
-def get_x_y(data_dir):
-    x=Data_Processing.get_x(data_dir)
-    y=Data_Processing.get_cov(data_dir)
-    return x,y
-model=LSTM()
-data,target=get_x_y(data_dir)
-criterion = nn.BCELoss()
-optimizer = optim.Adam(model.parameters(), lr=lr)
-dataloader=get_dataloader(data,target)
+def load_samples_bitmaps(sample_dir,bitmap_file):
+    Samples,max_filesize=Data_Processing.get_x(sample_dir)
+    if os.path.exists(bitmap_file):
+        bitmaps=eval(open(bitmap_file,'r').read())
+    else:
+        bitmaps=Data_Processing.get_Bitmap_data_fast(bitmap_file)
+    return Samples,np.array(bitmaps)
 
+samples,bitmaps=load_samples_bitmaps(data_dir,bitmap_file)
+mean_bitmaps=bitmaps.sum()/len(bitmaps)
+std_bitmaps=bitmaps.std()
+bitmap_normalizer = transforms.Normalize(mean=mean_bitmaps, std=std_bitmaps)
+
+train_size, test_size = int(len(samples) * 0.8), len(samples) - int(len(samples) * 0.8)  
+train_sample_dataset, test_sample_dataset = random_split(samples, [train_size, test_size])
+train_bitmap_dataset, test_bitmap_dataset = random_split(bitmaps, [train_size, test_size])  
+print(train_size,test_size)
+
+Train_loader = get_dataloader(batch_size,train_sample_dataset,train_bitmap_dataset)
+Test_loader = get_dataloader(batch_size,test_sample_dataset,test_bitmap_dataset)
+# ##
+model=LSTMnet(in_dim=input_dim,hidden_dim=hidden_dim,n_layer=n_layer,n_class=n_class)
+model.to(device)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+train_losses=[]
+test_losses=[]
 # 训练模型
 for epoch in range(epochs):
-    for batch_data, batch_targets in dataloader:
+    model.train()
+    for batch_data, batch_targets in Train_loader:
+        batch_data,batch_targets=batch_data.to(device),batch_targets.to(device)
         optimizer.zero_grad()
-        outputs = model(batch_data)
+        outputs = model(batch_data).reshape(-1,1,1)
+        outputs = outputs.to(device)
         loss = criterion(outputs, batch_targets)
         loss.backward()
         optimizer.step()
+    train_losses.append(loss.item())
+    print('outputs: {}'.format(outputs*std_bitmaps+mean_bitmaps))
     print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, epochs, loss.item()))
+    # test
+    size = len(Test_loader.dataset)  
+    num_batches = len(Test_loader)  
+
+    model.eval()  
+    test_loss = 0 
+    with torch.no_grad():  
+        for X, y in Test_loader:  
+            X, y = X.to(device), y.to(device)  
+            pred = model(X).reshape(-1,1,1)
+            test_loss += criterion(pred, y.long()).item()  
+    test_loss /= num_batches  
+    test_losses.append(test_loss)
+    print(f"Test Error:  Avg loss: {test_loss:>8f} \n")
+
+torch.save(model,'Mut_Opt')
+
+# draw the picture
+plt.plot(train_losses, label='train_losses', alpha=0.5)
+plt.plot(test_losses, label='test_losses', alpha=0.5)
+plt.title("Training Losses")
+plt.legend()
+plt.show()
